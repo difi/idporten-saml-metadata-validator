9e7fc4c833ef310662528f7809a3dd8bf42cf8ca
Sam O'Brien <sobrien@vsware.ie>
2017-07-17 12:21:20 +0100

    English language - todo: Norwegian & English localsation

--- src/main/java/no/difi/application/Application.java
+++ src/main/java/no/difi/application/Application.java
@@ -2,9 +2,6 @@ package no.difi.application;
 
 import no.difi.config.MetadataConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 public class Application {
     public static final String ROOT=System.getProperty("user.dir");
